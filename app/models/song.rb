be1819b3153dba67a22394009a33262655ccf69f
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-02-08 17:58:37 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,5 +8,11 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
+    if artist_id
+      artist.update(name: name) 
+    else
+      artist = Artist.find_or_create_by(name: name)
+      self.artist_id = artist.id
+    end
   end
 end
